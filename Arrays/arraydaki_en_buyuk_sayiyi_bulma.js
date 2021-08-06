503d514e28482931338115e1ddf5171f9de7c6a7
balioglum <balioglum@gmail.com>
2021-08-07 00:18:33 +0300

    array_enbuyuk_sayi

---
+++ Arrays/arraydaki_en_buyuk_sayiyi_bulma.js
@@ -0,0 +1,40 @@
+
+// birinci yöntem 
+let grades = [95, 4, 4, 21, 34, 65, 87, 9, 99, 23, 4];
+
+let largest = grades[0];
+
+grades.forEach(findGreatest);
+
+function findGreatest(grade) {
+    if (grade > largest) largest = grade;
+}
+
+console.log(largest);
+
+console.log(grades.indexOf(largest));
+
+
+// ikinci yöntem
+let largest = grades[0];
+for (let i = 0; i < grades.length; i++) {
+
+    if (grades[i] > largest) {
+        largest = grades[i]
+    }
+};
+
+console.log(grades.indexOf(largest));
+
+// üçüncü yöntem
+var
+    a = [2, 3, 4, 2, 1, 9],
+    largest = 0;
+
+a.forEach(function (value) {
+    console.log('current array element value: ' + value + '.  Is it larger than the largest?', (largest > value));
+    if (largest < value) largest = value;
+    console.log('largest value now is: ', largest);
+});
+console.log('largest is changed here, because it was not redefined inside the function of the forEach method: ', largest, '.\n 
+But it was defined within its context.'); */
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 #My all JS Projects and Scripts
 
 Project 1: Find the largest item in an array
+
+## subheader
