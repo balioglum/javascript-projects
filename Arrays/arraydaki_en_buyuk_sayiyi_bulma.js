e0f7fc1b240ea07219a1ded500185278790c21f7
Mehmet Balioglu <balioglum@gmail.com>
2021-08-09 17:50:32 +0300

    various3

--- Arrays/arraydaki_en_buyuk_sayiyi_bulma.js
+++ Arrays/arraydaki_en_buyuk_sayiyi_bulma.js
@@ -15,7 +15,7 @@ console.log(largest);
 console.log(grades.indexOf(largest));
 
 
-/* -------------------------------
+/* ----------------------------
 // ikinci yöntem
 let largest = grades[0];
 for (let i = 0; i < grades.length; i++) {
