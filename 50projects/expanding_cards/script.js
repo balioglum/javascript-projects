3a222dce5dfff589c85394b080fd1d6e837c3f4d
Mehmet Balioglu <balioglum@gmail.com>
2021-08-10 21:44:31 +0300

    various

--- 50projects/expanding_cards/script.js
+++ 50projects/expanding_cards/script.js
@@ -1,15 +1,19 @@
 // querySelector= sadece ilk bulduğu elementi seçer, querySelectorAll= hepsini seçip bir node list oluşturuyor. array'e benziyor
 
-    const panels= document.querySelectorAll(".panel");
+const panels = document.querySelectorAll(".panel");
 
-    panels.forEach( (panel) => {
-        panel.addEventListener ('click', () => {
-            removeAllActiveClasses();
-            panel.classList.add('active');
-        })
+panels.forEach((panel) => {
+    panel.addEventListener('click', () => {
+        removeAllActiveClasses(); //tüm panellerden active class'ı siliyor
+        panel.classList.add('active'); //tıklanan panele  active class'ı ekliyor
     })
+})
 
-    function removeAllActiveClasses() {
-        panels.forEach(panel=>{ panel.classList.remove('active');})
-     
-    }
+function removeAllActiveClasses() {
+    panels.forEach(panel => {panel.classList.remove('active'); })
+
+}
+
+// forEach "array metodu" ile, seçilen tüm classlara aynı anda addEventListener ekliyoruz.
+// tek event listenera aynı anda birçok sonuç ekleyebiliyoruz. 
+//burada removeallactiveclasses ile class sildik,sonra tıklanan class'a .active class'ı ekledik.
