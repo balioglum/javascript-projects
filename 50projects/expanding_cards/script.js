eaa067f6c00cfd6630cc3ef191a2b91b474787c6
Mehmet Balioglu <balioglum@gmail.com>
2021-08-10 04:58:24 +0300

    expansive cards

---
+++ 50projects/expanding_cards/script.js
@@ -0,0 +1,14 @@
+// querySelector= sadece ilk bulduğu elementi seçer, querySelectorAll= hepsini seçip bir node list oluşturuyor. array'e benziyor
+const panels = document.querySelectorAll(".panel");
+
+panels.forEach( (panel) => {
+    panel.addEventListener('click', () => {
+    removeAllActiveClasses();
+    panel.classList.add('active')
+    }) } );
+
+    function removeAllActiveClasses() {
+        panels.forEach( (panel) => {
+            panel.classList.remove('active')
+        })
+    }
