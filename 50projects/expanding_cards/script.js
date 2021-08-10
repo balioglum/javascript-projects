a8b2b7dc649dba89f79d577aefd82fd361f39c81
Mehmet Balioglu <balioglum@gmail.com>
2021-08-10 06:05:52 +0300

    study

--- 50projects/expanding_cards/script.js
+++ 50projects/expanding_cards/script.js
@@ -1,14 +1,15 @@
 // querySelector= sadece ilk bulduğu elementi seçer, querySelectorAll= hepsini seçip bir node list oluşturuyor. array'e benziyor
-const panels = document.querySelectorAll(".panel");
 
-panels.forEach( (panel) => {
-    panel.addEventListener('click', () => {
-    removeAllActiveClasses();
-    panel.classList.add('active')
-    }) } );
+    const panels= document.querySelectorAll(".panel");
+
+    panels.forEach( (panel) => {
+        panel.addEventListener ('click', () => {
+            removeAllActiveClasses();
+            panel.classList.add('active');
+        })
+    })
 
     function removeAllActiveClasses() {
-        panels.forEach( (panel) => {
-            panel.classList.remove('active')
-        })
+        panels.forEach(panel=>{ panel.classList.remove('active');})
+     
     }
